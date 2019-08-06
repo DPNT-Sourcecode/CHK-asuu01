400867ab522bd91fc07372a516779d351842e50e
sbx_user1051 <sbx_user1051@169.254.21.29>
2019-08-06 20:07:45 +0000

    Tue Aug 06 20:07:45 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    pass
+    shoppingList = skus.split()
+
+    priceTable = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
+
+    countOfA = 0
+    countOfB = 0
+
+    total = 0
+
+    for item in shoppingList:
+        pass
+
+checkout('AAA')
 
